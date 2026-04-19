80159c0f04bb2feb9468b36beb323e695e568307
VuDaiSon <cocokoco108@gmail.com>
2026-04-19 16:20:21 +0700

    fix

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,5 +1,5 @@
 export const buildImageUrl = (url) => {
   if (!url) return "";
 
-  return url.replace("/upload/", "/upload/f_auto,q_auto,w_800/");
+  return url.replace("/upload/", "/upload/f_auto,q_auto,c_limit,w_800/");
 };
